b1d563d40b7c35961976df06213de0869c45af42
ilgnerbecheleni <ilgner.gui@gmail.com>
2025-04-22 16:04:30 -0300

    5000

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ io.on("connection", (socket) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
 })
